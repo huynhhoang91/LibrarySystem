cf318a5739db94f58a0f55c20b7cd4a5ed6052e6
Benny Lam <benny3946@hotmail.com>
2019-05-16 13:55:54 -0700

    Update populate table.sql

--- LibraryManagementSystem/library dataset/populate table.sql
+++ LibraryManagementSystem/library dataset/populate table.sql
@@ -2,10 +2,9 @@
 insert into books (ID, booksName, publisher,cover,coverFull)
 SELECT ISBN,`Book-Title`,Publisher,`Image-URL-M`,`Image-URL-L` FROM library.bxbooks;
 
-insert into writes (booksID, authorName, publishingDate)
-SELECT ISBN,`Book-Author`, STR_TO_DATE(`Year-Of-Publication`, '%Y') FROM library.bxbooks;
-
 insert into author (authorName)
 SELECT `Book-Author` FROM library.bxbooks
 group by `Book-Author`;
 
+insert into writes (booksID, authorName, publishingDate)
+SELECT ISBN,`Book-Author`, STR_TO_DATE(`Year-Of-Publication`, '%Y') FROM library.bxbooks;
