83efb18cbea2a074c6c3191e56ac334b088e600e
Benny Lam <benny3946@hotmail.com>
2019-05-15 18:22:55 -0700

    Update library.sql
    schema update

--- LibraryManagementSystem/library dataset/library.sql
+++ LibraryManagementSystem/library dataset/library.sql
@@ -1,19 +1,21 @@
 CREATE TABLE Books (
     ID char(13) primary key,
     booksName text,
+    publisher varchar(255),
     Copys int default 5,
     borrows int default 0,
     category varchar(255) ,
     price float,
-    cover text
+    cover text,
+    coverFull text
 );
 
 create table Users(
-	ID int primary key,
-    userPassword varchar(255),
+	username varchar(255) binary primary key,
+    userPassword varchar(255) binary,
     firstName varchar(255),
     lastName varchar(255),
-    email varchar(255)
+    email varchar(255) unique
     
 );
 
@@ -24,7 +26,7 @@ create table Author(
 );
 
 create table Equipment(
-	ID int primary key,
+	ID int primary key AUTO_INCREMENT,
     equipName varchar(255),
     totalNumber int,
     borrows int
@@ -32,26 +34,28 @@ create table Equipment(
 
 create table Admins(
 	ID int primary key,
-    authorsName varchar(255),
+    adminName varchar(255),
     adminPassword varchar(255)
 );
 
 create table Writes(
 	booksID char(13), FOREIGN KEY (booksID) REFERENCES Books(ID),
-    authorsName varchar(255), FOREIGN KEY (authorsName) REFERENCES Author(authorName),
+    authorName varchar(255), FOREIGN KEY (authorName) REFERENCES Author(authorName),
     publishingDate date
 );
 
 create table BorrowBook(
-	userID int, FOREIGN KEY (userID) REFERENCES Users(ID),
+	username  varchar(255) binary, FOREIGN KEY (username) REFERENCES Users(username),
     booksID char(13), FOREIGN KEY (booksID) REFERENCES Books(ID),
     borrowDate date,
-    returnDate date
+    returnDate date,
+    returned boolean default 0 
 );
 
 create table BorrowEquipment(
-	userID int, FOREIGN KEY (userID) REFERENCES Users(ID),
+	username  varchar(255)binary, FOREIGN KEY (username) REFERENCES Users(username),
     equipmentID int, FOREIGN KEY (equipmentID) REFERENCES Equipment(ID),
 	borrowDate date,
-    returnDate date
+    returnDate date,
+    returned boolean default 0 
 );
